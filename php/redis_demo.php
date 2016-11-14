25362206b2a861b643bf93f8b676ecf520af0adc
Your Name <you@example.com>
2016-11-14 10:50:53 +0800

    test

--- php/redis_demo.php
+++ php/redis_demo.php
@@ -3,22 +3,8 @@ use Workerman\Worker;
 use Workerman\Protocols\Http;
 use WebWorker\Libs\Mredis;
 
-//判断系统
-if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-    require_once __DIR__.'/../../workerman-for-win/Autoloader.php';
-}else {
-    require_once __DIR__.'/../../workerman/Autoloader.php';
-    // 检查扩展
-    if(!extension_loaded('pcntl'))
-    {
-        exit("Please install pcntl extension. See http://doc3.workerman.net/install/install.html\n");
-    }
+require 'vendor/autoload.php';
 
-    if(!extension_loaded('posix'))
-    {
-        exit("Please install posix extension. See http://doc3.workerman.net/install/install.html\n");
-    }
-}
 
 $app = new WebWorker\App("http://0.0.0.0:1215");
 
